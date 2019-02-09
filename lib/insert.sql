3fcef9ae3fbeb5acf043ec6d50b300d4ab259c08
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-09 23:44:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO users (id, name, age) VALUES
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
