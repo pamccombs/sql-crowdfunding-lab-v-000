0c65912882f04eaee77d700709cae26287576df5
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-09 23:47:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,25 +1,7 @@
-INSERT INTO users (id, name, age) VALUES
-
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
-(id, name, age)
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
+(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
