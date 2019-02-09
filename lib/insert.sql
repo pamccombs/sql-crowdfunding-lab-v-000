561369808c9260348f385cd50b7a7ff59201c42a
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-09 23:46:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,25 @@
 INSERT INTO users (id, name, age) VALUES
 
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+(id, name, age)
+
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
